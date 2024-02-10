20ed529747846d2abe30ab7ec0d640ed582a2668
James Clarke <jc1222@canterbury.ac.uk>
2024-02-10 04:34:34 +0000

    Wrote tests for json encoding/decoding.

--- src/formats/json.rs
+++ src/formats/json.rs
@@ -23,3 +23,45 @@ where
     *data = serde_json::from_reader(reader).map_err(|_| DatabaseError::SerdeError)?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+    use std::fs;
+    use std::io::Read;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn test_serialize() {
+        let mut data = HashMap::new();
+        data.insert("key1", "value1");
+        data.insert("key2", "value2");
+
+        let mut temp_file = NamedTempFile::new().expect("Failed to create temporary file");
+        serialize(&mut temp_file, &data).expect("Failed to serialize data");
+
+        let mut contents = String::new();
+        temp_file
+            .reopen()
+            .unwrap()
+            .read_to_string(&mut contents)
+            .unwrap();
+        assert!(contents.contains("\"key1\":\"value1\""));
+        assert!(contents.contains("\"key2\":\"value2\""));
+    }
+
+    #[test]
+    fn test_deserialize() {
+        let json_content = r#"{"key1":"value1","key2":"value2"}"#;
+        let temp_file = NamedTempFile::new().expect("Failed to create temporary file");
+        fs::write(temp_file.path(), json_content).expect("Failed to write to temporary file");
+
+        let mut data: HashMap<String, String> = HashMap::new();
+        let file = fs::File::open(temp_file.path()).unwrap();
+        deserialize(file, &mut data).expect("Failed to deserialize data");
+
+        assert_eq!(data.get("key1"), Some(&"value1".to_string()));
+        assert_eq!(data.get("key2"), Some(&"value2".to_string()));
+    }
+}
